82aea7a2834c4233fe3d6ce4bea73deb1b93f990
Zain Majid <zainarbaazmajid@Zains-MacBook-Pro.local>
2019-05-24 01:08:13 +0800

    whatever

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+   
+        <p>
+      Zain's Portfolio
+        </p>
+
+        <p>Hi! My name is Zain Majid and I am an intended IMB Major at NYU Shanghai. My two years</p>
+        
+        <div className="me"></div>
+  
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
